a9c25ad98ae6a4af364835d5f4d8dc23d83cc479
whiteboarddan <macnish.daniel@gmail.com>
2018-01-03 18:23:03 +1100

    split raspi-run script into seperate run and build scripts

---
+++ raspi-build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo docker build . -t cartoonify:latest
--- raspi-run.sh
+++ raspi-run.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-sudo docker build . -t cartoonify:latest
 sudo docker run --entrypoint /bin/bash -p 8081:8081 -it cartoonify
