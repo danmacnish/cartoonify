f5695fac50b5945c57c12b8a437ff60b172ad935
whiteboarddan <macnish.daniel@gmail.com>
2018-01-06 00:00:29 +1100

    revert to mobilenet model

--- cartoonify/app/image_processor/imageprocessor.py
+++ cartoonify/app/image_processor/imageprocessor.py
@@ -12,9 +12,8 @@ import click
 
 
 root = Path(__file__).parent
-tensorflow_model_name = 'faster_rcnn_inception_v2_coco_2017_11_08'
+tensorflow_model_name = 'ssd_mobilenet_v1_coco_2017_11_17'
 model_path = root / '..' / '..' / 'downloads' / 'detection_models' / tensorflow_model_name / 'frozen_inference_graph.pb'
-
 
 class ImageProcessor(object):
     """performs object detection on an image
