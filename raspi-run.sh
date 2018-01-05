a6d61cedf1467b3fa953f49782887fb6acf85d37
whiteboarddan <macnish.daniel@gmail.com>
2018-01-05 22:50:42 +1100

    mount to absolute path

--- raspi-run.sh
+++ raspi-run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo docker run --mount type=bind,source=$(pwd)/cartoonify,target=~/cartoonify --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem --privileged --entrypoint /bin/bash -p 8081:8081 -it cartoonify
+sudo docker run --mount type=bind,source=$(pwd)/cartoonify,target=/cartoonify --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem --privileged --entrypoint /bin/bash -p 8081:8081 -it cartoonify
