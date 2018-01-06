54dcdd522ac12523afce381ce6db00e5aaee415b
whiteboarddan <macnish.daniel@gmail.com>
2018-01-06 15:58:32 +1100

    user and expose port 9000 for ws

--- raspi-run.sh
+++ raspi-run.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-sudo docker run --mount type=bind,source=$(pwd)/cartoonify,target=/cartoonify --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem --privileged --entrypoint /bin/bash -p 8081:8081 -it cartoonify
+sudo docker run --mount type=bind,source=$(pwd)/cartoonify,target=/cartoonify \
+ --device /dev/ttyAMA0:/dev/ttyAMA0 \
+ --device /dev/mem:/dev/mem \
+ --privileged \
+ --entrypoint /bin/bash \
+ -p 8081:8081 \
+ -p 9000:9000 \
+ -it \
+ cartoonify
