fc55c79b7c8fe88bc92e8b7a75c0741f17c3789f
whiteboarddan <macnish.daniel@gmail.com>
2018-01-06 12:47:25 +1100

    update paths for asset download

--- raspi-build.sh
+++ raspi-build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 pip install -r raspi_install/download_assets_requirements.txt
-sudo pip install -e ./cartoonify
 python raspi_install/download_assets.py
 sudo docker build . -t cartoonify:latest
