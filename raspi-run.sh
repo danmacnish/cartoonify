7558a8a470977a42f2f95934b02a64e0bd683141
whiteboarddan <macnish.daniel@gmail.com>
2018-01-14 12:26:58 +1100

    explicitly tell docker to run container on the host network, so that container can be accessed at 127.0.0.1

--- raspi-run.sh
+++ raspi-run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 sudo docker run -d \
  --mount type=bind,source=$(pwd)/cartoonify,target=/cartoonify \
+ --network=host \
  --device /dev/ttyAMA0:/dev/ttyAMA0 \
  --device /dev/mem:/dev/mem \
  --privileged \
- -p 127.0.0.1:8081:8081 \
- -p 127.0.0.1:8082:8082 \
+ -p 8081:8081 \
+ -p 8082:8082 \
  -w /cartoonify \
  cartoonify
