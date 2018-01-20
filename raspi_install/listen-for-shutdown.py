f6a52516350237ead66ff0787c2f537b1105238e
whiteboarddan <macnish.daniel@gmail.com>
2018-01-20 11:41:00 +1100

    add shutdown scripts

---
+++ raspi_install/listen-for-shutdown.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+import RPi.GPIO as GPIO
+import subprocess
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.wait_for_edge(3, GPIO.FALLING)
+
+subprocess.call(['shutdown', '-h', 'now'], shell=False)
---
+++ raspi_install/listen-for-shutdown.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+### BEGIN INIT INFO
+# Provides:          listen-for-shutdown.py
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+### END INIT INFO
+
+# If you want a command to always run, put it here
+
+# Carry out specific functions when asked to by the system
+case "$1" in
+  start)
+    echo "Starting listen-for-shutdown.py"
+    /usr/local/bin/listen-for-shutdown.py &
+    ;;
+  stop)
+    echo "Stopping listen-for-shutdown.py"
+    pkill -f /usr/local/bin/listen-for-shutdown.py
+    ;;
+  *)
+    echo "Usage: /etc/init.d/listen-for-shutdown.sh {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
