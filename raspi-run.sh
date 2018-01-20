d8ebbe35e8277eeca2053ac77f8e38b9e7520217
whiteboarddan <macnish.daniel@gmail.com>
2018-01-21 10:51:46 +1100

    install shutdown scripts in host OS instead of docker image

--- raspi-run.sh
+++ raspi-run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo docker run -d --init \
+sudo docker run -d \
  --mount type=bind,source=$(pwd)/cartoonify,target=/cartoonify \
  --device /dev/ttyAMA0:/dev/ttyAMA0 \
  --device /dev/mem:/dev/mem \
--- raspi_install/listen-for-shutdown.sh
+++ raspi_install/listen-for-shutdown.sh
@@ -14,14 +14,14 @@
 case "$1" in
   start)
     echo "Starting listen-for-shutdown.py"
-    ./listen-for-shutdown.py &
+    /usr/local/bin/listen-for-shutdown.py &
     ;;
   stop)
     echo "Stopping listen-for-shutdown.py"
-    pkill -f ./listen-for-shutdown.py
+    pkill -f /usr/local/bin/listen-for-shutdown.py
     ;;
   *)
-    echo "Usage: ./listen-for-shutdown.sh {start|stop}"
+    echo "Usage: /etc/init.d/listen-for-shutdown.sh {start|stop}"
     exit 1
     ;;
 esac
