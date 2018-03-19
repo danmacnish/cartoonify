40f1e24f253718042faf9021393686253a19c4aa
whiteboarddan <macnish.daniel@gmail.com>
2018-03-19 19:15:20 +1100

    set printer in startup script

--- raspi_install/startup.sh
+++ raspi_install/startup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+cd /zj-58
+sudo lpadmin -p ZJ-58 -E -v serial:/dev/ttyUSB0?baud=9600 -m zjiang/ZJ-58.ppd
+sudo lpoptions -d ZJ-58
 cd /cartoonify
 sudo pip install -e .
 cartoonify --raspi-headless --raspi-gpio
