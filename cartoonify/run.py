11b58bd4a4e3a0694d6760ac2b220dcbd25f07ea
whiteboarddan <macnish.daniel@gmail.com>
2018-03-28 21:04:33 +1100

    don't print cartoon

--- cartoonify/run.py
+++ cartoonify/run.py
@@ -59,7 +59,7 @@ def run(camera, gui, raspi_headless, batch_process, raspi_gpio):
             if raspi_headless:
                 while True:
                     if app.gpio.get_capture_pin():
-                        app.run(print_cartoon=True)
+                        app.run(print_cartoon=False)
                         time.sleep(0.02)
             if camera:
                 if click.confirm('would you like to capture an image?'):
