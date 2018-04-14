6a3894624ca218aff7453e543f507ac71724f23e
whiteboarddan <macnish.daniel@gmail.com>
2018-04-14 13:34:06 +1000

    print to /dev/ttyS0

--- raspi_install/startup.sh
+++ raspi_install/startup.sh
@@ -2,7 +2,7 @@
 cd /cartoonify/zj-58
 make
 sudo ./install
-sudo lpadmin -p ZJ-58 -E -v serial:/dev/serial0?baud=9600 -m zjiang/ZJ-58.ppd
+sudo lpadmin -p ZJ-58 -E -v serial:/dev/ttyS0?baud=9600 -m zjiang/ZJ-58.ppd
 sudo lpoptions -d ZJ-58
 cd /cartoonify
 sudo pip install -e .
